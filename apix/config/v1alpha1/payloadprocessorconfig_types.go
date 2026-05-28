0ee700b66867b670c219b23dc62f923d798cfce1
ronenkat <16743404+ronenkat@users.noreply.github.com>
2026-05-28 17:49:02 +0300

    fix merge issue
    Signed-off-by: ronenkat <16743404+ronenkat@users.noreply.github.com>

--- apix/config/v1alpha1/payloadprocessorconfig_types.go
+++ apix/config/v1alpha1/payloadprocessorconfig_types.go
@@ -79,10 +79,6 @@ func (cfg PayloadProcessorConfig) String() string {
 	if cfg.ProfilePicker != nil {
 		fmt.Fprintf(contents, ", ProfilePicker: %v", cfg.ProfilePicker)
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> add-notification-source-config
 	fmt.Fprintf(contents, ", Profiles: %v", cfg.Profiles)
 	if cfg.PostProcessing != nil {
 		fmt.Fprintf(contents, ", PostProcessing: %v", cfg.PostProcessing)
